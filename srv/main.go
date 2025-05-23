7742cc80e0c1a2c01f3975c97bc5ed20c27fa5db
Jamie-Poeffel <Jamie.poeffel@gmail.com>
2025-05-23 13:31:56 +0200

    [feature] Logging added (#9) | Files: srv/logger/logger.go, srv/main.go, srv/test.txt Logging added in go Request Logging (#14), Error Logging (#15), Logger file (#12) and Logger class (#13)

--- srv/main.go
+++ srv/main.go
@@ -1,7 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+
+	"srv/aibot/logger"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	http.HandleFunc("/", logger.RequestHandler)
+	http.HandleFunc("/error", logger.ErrorHandler)
+
+	fmt.Println("Server läuft auf http://localhost:8080")
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		logger.ErrorLog.Printf("Fehler beim Starten des Servers: %v", err)
+	}
 }
