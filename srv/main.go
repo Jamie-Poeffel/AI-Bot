54ff6056503c4464ee18c4f4b2e4c86917f8e47c
Jamie-Poeffel <Jamie.poeffel@gmail.com>
2025-05-30 13:29:33 +0200

    [feature] Implement user registration and login functionality with MongoDB integration

--- srv/main.go
+++ srv/main.go
@@ -6,10 +6,13 @@ import (
 
 	"srv/aibot/logger"
 	"srv/aibot/controllers"
+	"srv/aibot/db"
 )
 
 func main() {
-	http.HandleFunc("/newUser", logger.RequestHandler(controllers.User))
+	db.Connect()
+	http.HandleFunc("/newUser", logger.RequestHandler(controllers.Register))
+	http.HandleFunc("/login", logger.RequestHandler(controllers.Login))
 
 	fmt.Println("Server läuft auf http://localhost:8080")
 	if err := http.ListenAndServe(":8080", nil); err != nil {
